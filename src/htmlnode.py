f1e170fc31a3aca135bc1fcb5301499c5ed78dd1
kaager <github@kaager.net>
2026-05-21 20:25:25 +0200

    Ch2, L5

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -38,3 +38,20 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"Tag: {self.tag}, Value: {self.value}, Props: {self.props}"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("'tag' can't be None")
+        if self.children is None:
+            raise ValueError("no children for this parent")
+        
+        children_string = ""
+        for child in self.children:
+            children_string += child.to_html()
+        
+        return f"<{self.tag}{self.props_to_html()}>{children_string}</{self.tag}>"
