461ad57637b71ec4b4e452e6c743df2e855104e7
kaager <github@kaager.net>
2026-05-27 14:13:20 +0200

    CH5, L1

--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 
 def main():
-    some_textnode_object = TextNode("This is some anchor text", TextType.LINKS, "https://boot.dev")
-    print(some_textnode_object)
+    static_path = "./static"
+    public_path = "./public"
+    prepare_static_to_public(static_path, public_path)
+
+
+def prepare_static_to_public(static_path, public_path):
+    if not os.path.exists(static_path):
+        raise Exception("Path does not exist (static)")
+    
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+    os.mkdir(public_path)
+
+    copy_static_to_public(static_path, public_path)
+
+
+def copy_static_to_public(static_path, public_path):
+    items = os.listdir(static_path)
+
+    for item in items:
+        file_path = os.path.join(static_path, item)
+        public_file_path = os.path.join(public_path, item)
+        if os.path.isfile(file_path):
+            print(f"Copying {file_path} -> {public_file_path}")
+            shutil.copy(file_path, public_file_path)
+        elif os.path.isdir(file_path):
+            os.mkdir(public_file_path)
+            copy_static_to_public(file_path, public_file_path)
 
 main()
